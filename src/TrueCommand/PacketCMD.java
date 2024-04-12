ae745b5ad74eb76f0f3d80ca14429acc04b7b2ab
alexandrage <alexandrage@mail.ru>
2024-04-12 19:22:28 +0300

    update

--- src/TrueCommand/PacketCMD.java
+++ src/TrueCommand/PacketCMD.java
@@ -3,6 +3,9 @@ package TrueCommand;
 import TrueCommand.wrapper.WrapperPlayClientChatCommand;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.PacketType;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import org.bukkit.plugin.Plugin;
 import TrueCommand.utils.CommandUtils;
 import com.comphenix.protocol.events.PacketAdapter;
@@ -20,15 +23,20 @@ public class PacketCMD extends PacketAdapter {
 		WrapperPlayClientChatCommand packet = new WrapperPlayClientChatCommand(e.getPacket());
 		String msg = packet.getMessage();
 		if (msg.length() > 1) {
-			String cmd = msg.split(" ")[0];
+			List<String> list = new LinkedList<String>(Arrays.asList(msg.split(" ")));
+			String cmd = list.get(0);
 			if (this.utils.get(cmd) != null) {
-				packet.setMessage(msg);
 				return;
 			}
 			if (this.utils.has(String.valueOf(cmd.toCharArray()[0]))) {
 				String rcmd = this.utils.repl(cmd);
 				if (this.utils.get(rcmd) != null) {
-					packet.setMessage(rcmd);
+					if (list.size() > 1) {
+						list.remove(0);
+						packet.setMessage(rcmd + " " + this.utils.repl(String.join(" ", list)));
+					} else {
+						packet.setMessage(rcmd);
+					}
 				}
 			}
 		}
--- src/TrueCommand/PacketChat.java
+++ src/TrueCommand/PacketChat.java
@@ -5,7 +5,9 @@ import TrueCommand.wrapper.WrapperPlayClientChatCommand;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
-
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import org.bukkit.plugin.Plugin;
 import TrueCommand.utils.CommandUtils;
 import com.comphenix.protocol.events.PacketAdapter;
@@ -17,30 +19,36 @@ public class PacketChat extends PacketAdapter {
 		super(plugin, type);
 		this.utils = commandUtils;
 	}
-	
+
 	@Override
-    public void onPacketReceiving(PacketEvent e) {
-        WrapperPlayClientChat packet = new WrapperPlayClientChat(e.getPacket());
-        WrapperPlayClientChatCommand packet2 = new WrapperPlayClientChatCommand();
-        String msg = packet.getMessage();
-        if (msg.length() > 1 && msg.startsWith(".")) {
-            String cmd = msg.split(" ")[0].substring(1);
-            if (this.utils.get(cmd) != null) {
-            	packet2.setMessage(cmd);
-            	packet2.getHandle().getModifier().write(1, packet.getHandle().getModifier().read(1));
-            	ProtocolLibrary.getProtocolManager().receiveClientPacket(e.getPlayer(), packet2.getHandle(), true);
-            	e.setCancelled(true);
-                return;
-            }
-            if (this.utils.has(String.valueOf(cmd.toCharArray()[0]))) {
-                String rcmd = this.utils.repl(cmd);
-                if (this.utils.get(rcmd) != null) {
-                	packet2.setMessage(rcmd);
-                	packet2.getHandle().getModifier().write(1, packet.getHandle().getModifier().read(1));
-                	ProtocolLibrary.getProtocolManager().receiveClientPacket(e.getPlayer(), packet2.getHandle(), true);
-                	e.setCancelled(true);
-                }
-            }
-        }
-    }
+	public void onPacketReceiving(PacketEvent e) {
+		WrapperPlayClientChat packet = new WrapperPlayClientChat(e.getPacket());
+		WrapperPlayClientChatCommand packet2 = new WrapperPlayClientChatCommand();
+		String msg = packet.getMessage();
+		if (msg.length() > 1 && msg.startsWith(".")) {
+			List<String> list = new LinkedList<String>(Arrays.asList(msg.split(" ")));
+			String cmd = list.get(0).substring(1);
+			if (this.utils.get(cmd) != null) {
+				packet2.setMessage(msg);
+				packet2.getHandle().getModifier().write(1, packet.getHandle().getModifier().read(1));
+				ProtocolLibrary.getProtocolManager().receiveClientPacket(e.getPlayer(), packet2.getHandle(), true);
+				e.setCancelled(true);
+				return;
+			}
+			if (this.utils.has(String.valueOf(cmd.toCharArray()[0]))) {
+				String rcmd = this.utils.repl(cmd);
+				if (this.utils.get(rcmd) != null) {
+					if (list.size() > 1) {
+						list.remove(0);
+						packet2.setMessage(rcmd + " " + this.utils.repl(String.join(" ", list)));
+					} else {
+						packet2.setMessage(rcmd);
+					}
+					packet2.getHandle().getModifier().write(1, packet.getHandle().getModifier().read(1));
+					ProtocolLibrary.getProtocolManager().receiveClientPacket(e.getPlayer(), packet2.getHandle(), true);
+					e.setCancelled(true);
+				}
+			}
+		}
+	}
 }
